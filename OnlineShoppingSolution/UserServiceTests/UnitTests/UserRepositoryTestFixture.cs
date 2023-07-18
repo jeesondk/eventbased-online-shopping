8c1b95042beabf9fef427cedf8b9aa50979b8db8
Jesper Jensen <jesper.jensen@keemail.me>
2023-07-18 12:12:42 +0200

    test: added user repository tests

---
+++ OnlineShoppingSolution/UserServiceTests/UnitTests/UserRepositoryTestFixture.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using UserService.Context;
+using UserService.Models;
+
+namespace UserServiceTests.UnitTests;
+
+public class UserRepositoryTestFixture: IDisposable
+{
+
+    private readonly UserContext _userContext;
+    private readonly DbSet<UserModel> _dbSet;
+    
+    public DbSet<UserModel> GetDbSet() => _dbSet;
+    public UserContext GetUserContext() => _userContext;
+
+    public UserRepositoryTestFixture()
+    {
+        var users = new List<UserModel>
+        {
+            new UserModel
+            {
+            UserName = "TestUser1",
+            Email = "testuser1@test.com",
+            FirstName = "Test 1",
+            LastName = "Testsen",
+            Address = new AddressModel
+            {
+                Street = "Tester road 223",
+                City = "Test city",
+                PostalCode = "9999",
+                Country = "Test"
+            }
+        },
+            new UserModel
+            {
+                UserName = "TestUser2",
+                Email = "testuser2@test.com",
+                FirstName = "Test 2",
+                LastName = "Testsen",
+                Address = new AddressModel
+                {
+                    Street = "Tester road 223",
+                    City = "Test city",
+                    PostalCode = "9999",
+                    Country = "Test"
+                }
+            }
+        }.AsQueryable();
+
+        _dbSet = Substitute.For<DbSet<UserModel>, IQueryable<UserModel>>();
+        ((IQueryable<UserModel>)_dbSet).Provider.Returns(users.Provider);
+        ((IQueryable<UserModel>)_dbSet).Expression.Returns(users.Expression);
+        ((IQueryable<UserModel>)_dbSet).ElementType.Returns(users.ElementType);
+        ((IQueryable<UserModel>)_dbSet).GetEnumerator().Returns(users.GetEnumerator());
+
+        var dbContextOptions = new DbContextOptionsBuilder<UserContext>().UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        _userContext = new UserContext(dbContextOptions);
+        _userContext.Users.AddRange(users);
+        _userContext.SaveChanges();
+
+    }
+
+    public void Dispose()
+    {
+        
+    }
+    
+}
---
+++ OnlineShoppingSolution/UserServiceTests/UnitTests/UserRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using UserService.Models;
+using UserService.Users;
+
+namespace UserServiceTests.UnitTests;
+
+public class UserRepositoryTest: IClassFixture<UserRepositoryTestFixture>
+{
+    private IUserRepository _userRepository;
+    private readonly UserRepositoryTestFixture _fixture;
+
+
+    public UserRepositoryTest(UserRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+        _userRepository = new UserRepository(_fixture.GetUserContext());
+    }
+
+    [Fact]
+    public async Task GetUserByUserName_ShouldReturnUserModel()
+    {
+        // Arrange
+        string testUserName = "TestUser1";
+
+        // Act
+        var result = await _userRepository.GetUserByUserName(testUserName);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.UserName.Should().Be(testUserName);
+    }
+
+    [Fact]
+    public async Task CreateUser_ShouldAddNewUser()
+    {
+        // Arrange
+        var newUser = new UserModel
+        {
+            UserName = "TestUser3",
+            Email = "testuser3@test.com",
+            FirstName = "Test 3",
+            LastName = "Testsen",
+            Address = new AddressModel
+            {
+                Street = "Tester road 223",
+                City = "Test city",
+                PostalCode = "9999",
+                Country = "Test"
+            }
+        };
+        
+        // Act
+        await _userRepository.CreateUser(newUser);
+        
+        // Assert
+        _fixture.GetUserContext().Users.Contains(newUser).Should().BeTrue();
+    }
+}
