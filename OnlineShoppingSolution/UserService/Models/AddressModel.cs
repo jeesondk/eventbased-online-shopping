121bdb89e27e0ab2cce5fd6a33dede62da8a9910
Jesper Jensen <jesper.jensen@keemail.me>
2023-07-18 12:18:05 +0200

    feat: added address and entity and model

---
+++ OnlineShoppingSolution/UserService/Models/AddressModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Common.Domain.User.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserService.Models;
+
+[Table("Address", Schema = "Users")]
+[Index(nameof(UserId), IsUnique = true)]
+[PrimaryKey("UserId")]
+public class AddressModel
+{
+
+    [Key]
+    [ForeignKey("UserId")]
+    public Guid UserId { get; set; }
+    [MaxLength(256)]
+    public string Street { get; set; } = string.Empty;
+    [MaxLength(256)]
+    public string City { get; set; } = string.Empty;
+    [MaxLength(256)]
+    public string PostalCode { get; set; } = string.Empty;
+    [MaxLength(256)]
+    public string Country { get; set; } = string.Empty;
+
+    public Address ToEntity()
+    {
+        return new Address
+        {
+            Street = Street,
+            City = City,
+            PostalCode = PostalCode,
+            Country = Country
+        };
+    }
+}
