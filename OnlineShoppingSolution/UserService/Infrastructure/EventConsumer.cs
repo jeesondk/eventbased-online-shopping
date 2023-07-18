02a7052045749f02efb2cac366a2308fc173513e
Jesper Jensen <jesper.jensen@keemail.me>
2023-07-18 13:46:27 +0200

    feat: added implementation for EventConsumer

---
+++ OnlineShoppingSolution/UserService/Infrastructure/EventConsumer.cs
@@ -0,0 +1,34 @@
+﻿using Common.Domain.User.Events;
+using MassTransit;
+
+namespace UserService.Infrastructure;
+
+public class EventConsumer<T>: IConsumer<T> where T : class
+{
+    private readonly ILogger<EventConsumer<T>> _logger;
+    private readonly IUserService _service;
+    
+    public EventConsumer(ILogger<EventConsumer<T>> logger, IUserService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+    
+    public async Task Consume(ConsumeContext<T> context)
+    {
+        _logger.LogInformation("Consuming event type: {EventType}, with ID: {EventId}", context.Message.GetType(), context.MessageId);
+
+        switch (context.Message.GetType().Name)
+        {
+            case nameof(CreateUser):
+                await _service.CreateUser((context.Message as CreateUser)!);
+                break;
+            case nameof(GetUser):
+                await _service.GetUser((context.Message as GetUser)!);
+                break;
+            default: throw new ArgumentException("Unknowen Event type");
+        }
+
+        _logger.LogInformation("Done processing event");
+    }
+}
